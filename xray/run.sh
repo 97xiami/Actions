4f62c551b8e491ce3ac5fef4b36ba96d36feebc1
97xiami <2285623277cz@gmail.com>
2021-10-22 11:12:13 +0800

    up

--- xray/run.sh
+++ xray/run.sh
@@ -1,15 +1,33 @@
-#!​/bin/sh
-APP=/usr/bin/xray
-CONF=/etc/storage/xray/config.json
-NUM=​`​ps ​|​ grep -w ​$APP​ ​|​ grep -v grep ​|​ wc -l​`
-if​ [ ​"​$NUM​"​ ​-lt​ ​"​1​"​ ]​;​then
-  export​ SSL_CERT_FILE=/usr/lib/cacert.pem
-  $APP​ -config=​$CONF​ ​&
-elif​ [ ​"​$NUM​"​ ​-gt​ ​"​1​"​ ]​;​then
-  pgrep -f ​$APP​ ​|​ xargs ​kill​ -9
-  sleep 1s
-  export​ SSL_CERT_FILE=/usr/lib/cacert.pem
-  $APP​ -config=​$CONF​ ​&
-fi
-sh /etc/storage/xray/iptables.sh
-exit​ 0
+#!/usr/bin/bash
+
+func_start(){
+  xray -c /etc/storage/xray/config.json
+  iptables -t nat -N XRAY
+  iptables -t nat -A XRAY -d 0.0.0.0/8 -j RETURN
+  iptables -t nat -A XRAY -d 127.0.0.0/8 -j RETURN
+  iptables -t nat -A XRAY -d 10.0.0.0/24 -j RETURN
+  iptables -t nat -A XRAY -s 10.0.0.0/24 -p tcp -j REDIRECT --to-ports 12345
+  iptables -t nat -A PREROUTING -p tcp -j XRAY
+  iptables -t nat -A OUTPUT -p tcp -j XRAY
+}
+
+func_stop(){
+  iptables -t nat -D XRAY
+  killall -9 xray
+}
+
+case "$1" in
+start)
+  func_start
+;;
+stop)
+  func_stop
+;;
+restart)
+  func_stop
+  func_start
+;;
+*)
+  echo "Usage: $0 {start | stop | restart}"
+  exit 1
+esac
